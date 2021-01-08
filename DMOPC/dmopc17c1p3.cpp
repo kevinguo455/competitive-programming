5a8013feb5d3115b30f2a522262d5daaf3c93627
SirTechnical <kevin203@gmail.com>
2021-01-08 10:57:15 -0500

    Solved dmopc17c1p3

--- DMOPC/dmopc17c1p3.cpp
+++ DMOPC/dmopc17c1p3.cpp
@@ -16,23 +16,31 @@ using namespace std;
 #define vr(v) reverse(v.begin(), v.end())
 #define fi first
 #define se second
-#define fio cin.tie(0); cout.tie(0); ios::sync_with_stdio(0)
+#define fio cin.tie(0)->sync_with_stdio(0)
 typedef unsigned int ui;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef priority_queue<int> pqi;
 const int MOD = 1e9+7, INF = 0x3f3f3f3f, MAXN = 1e5+5; 
 
 struct Edge{int n; bool d;};
 vector<Edge> adj[MAXN];
+pii dist[MAXN];     // {dangerous roads, total roads}
+
+bool cmp(pii a, pii b) {
+    if (a.fi == b.fi) return a.se < b.se;
+    return a.fi < b.fi;
+}
+
+bool pqcmp(int a, int b) {
+    return !cmp(dist[a], dist[b]);
+}
 
 int main() {
     fio;
 
+    // init
     int nodes, edges; cin >> nodes >> edges;
     int a, b;
     bool c;
@@ -41,12 +49,25 @@ int main() {
         adj[a].pb({b,c});
         adj[b].pb({a,c});
     }
-    queue<int> q;
-    q.push(1);
-    while (!q.empty()) {
 
+    // dijkstra's
+    priority_queue<int, vector<int>, function<bool(int, int)>> pq(pqcmp);
+    pq.push(1);
+    f(i,2,nodes+1) dist[i] = {INF, INF};
+
+    while (!pq.empty()) {
+        int c = pq.top();
+        pq.pop();
+        for (Edge t:adj[c]) {
+            if (cmp({dist[c].fi+t.d, dist[c].se+1}, dist[t.n])) {
+                dist[t.n] = {dist[c].fi+t.d, dist[c].se+1};
+                pq.push(t.n);
+            }
+        }
     }
 
+    if (dist[nodes].fi == INF) cout << -1 << "\n";
+    else cout << dist[nodes].fi << " " << dist[nodes].se << "\n";
 
     return 0;
 }
